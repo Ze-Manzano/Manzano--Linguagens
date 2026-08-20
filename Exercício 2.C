21b95ae566372ca1de10dff71f26829ce66adbba
Ze-Manzano <jdepaolamanzano@gmail.com>
2026-08-20 10:45:45 -0300

    Update Exercício 2.C

--- Exercício 2.C
+++ Exercício 2.C
@@ -6,7 +6,6 @@ int main() {
     printf("Digite um valor positivo: ");
     scanf("%lf", &valor);
 
-    // %e exibe no formato nativo, ex: 1.234567e+03
     printf("Notacao cientifica: %e\n", valor);
 
     return 0;
