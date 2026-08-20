f1c35e5294621264d8332ba3cfe8f5d3ffcff064
Ze-Manzano <jdepaolamanzano@gmail.com>
2026-08-20 10:46:35 -0300

    Update Exercicio 1.C

--- Exercicio 1.C
+++ Exercicio 1.C
@@ -3,14 +3,12 @@
 int main() {
     int num1, num2;
 
-    // Leitura dos dois números inteiros
     printf("Digite o primeiro numero: ");
     scanf("%d", &num1);
 
     printf("Digite o segundo numero: ");
     scanf("%d", &num2);
 
-    // Impressão na ordem inversa da leitura
     printf("\nNumeros na ordem inversa:\n");
     printf("%d\n", num2);
     printf("%d\n", num1);
