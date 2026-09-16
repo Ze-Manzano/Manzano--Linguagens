f41c77ac46821b2859c30ce0544a52a15aff2d62
José Henrique Manzano <jdepaolamanzano@gmail.com>
2026-09-16 10:28:39 -0300

    Create Prova.C

---
+++ Prova.C
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main() {
+int A, B, C, D;
+
+printf("Digite seu primeiro valor: ");
+scanf("%d", &A);
+
+printf("Digite seu segundo valor: ");
+scanf("%d", &B);
+
+printf("Digite seu terceiro valor: ");
+scanf("%d", &C);
+
+printf("Digite seu quarto valor: ");
+scanf("%d,", &D);
+
+printf("Numeros permutados para a ordem C,A,D,B:\n");
+
+printf("C: %d\n", C); 
+printf("A: %d\n", A);
+printf("D: %d\n", D);
+printf("B: %d\n", B);
+
+return 0;
+
+}
